708de9572512eea379ae0c2543269d54a3b470f4
soroda <sdahalani@gmail.com>
2022-04-27 21:05:07 +0200

    Affichage du résultat parenthèsé & valorisé
    ne passe aucun test

--- EASY_equivalent-resistance-circuit-building.py
+++ EASY_equivalent-resistance-circuit-building.py
@@ -5,14 +5,23 @@ import math
 n = int(input())
 
 dico = {}
-equivalent = 0
+equivalent = ""
 
 for i in range(n):
     inputs = input().split()
     dico[inputs[0]] = float(inputs[1])
 
-print(dico, file=sys.stderr, flush=True)
-circuit = input().split()
-print(circuit, file=sys.stderr, flush=True)
+circuit = input()
+
+print(str(circuit.count('(')) + ' additions', file=sys.stderr, flush=True)
+print(str(circuit.count('[')) + ' divisions', file=sys.stderr, flush=True)
+
+for i in circuit.split():
+    if i in "([":
+        equivalent += i + ' '
+    elif i in "])":
+        equivalent += i + ' '
+    else :
+        equivalent += str(dico[i]) + ' '
 
 print(equivalent)
