2c8481287cbcd58e808bbf512cdfb4d736435767
soroda <sdahalani@gmail.com>
2022-05-05 07:29:58 +0200

    All tests ok

--- EASY_offset-arrays.py
+++ EASY_offset-arrays.py
@@ -1,44 +1,29 @@
 # https://www.codingame.com/ide/puzzle/offset-arrays
-import sys
-import re
 
 n = int(input())
-print(n, file=sys.stderr, flush=True)
 
 d = dict()
 
 for i in range(n):
-    assignment = input()
-    print(assignment, file=sys.stderr, flush=True)
-    
+    assignment = input()    
     first_index, last_index = assignment.split('[')[1].split(']')[0].split('..')
-    print(first_index + " et " + last_index, file=sys.stderr, flush=True)
-
-    array_identifier = assignment.split('[')[0]
-    print(array_identifier, file=sys.stderr, flush=True)
-    
+    array_identifier = assignment.split('[')[0]    
     values = assignment.split('=')[1].split()
-    print(values, file=sys.stderr, flush=True)
-
     k = 0
+    # constitution du dico avec tous les éléments
     for j in range(int(first_index),int(last_index)+1,1):
         tmp_key = array_identifier + '[' + str(j) + ']'
         d[tmp_key]=values[k]
         k += 1
         
-
-print(d, file=sys.stderr, flush=True)
 x = input()
 
-print(x, file=sys.stderr, flush=True)
-
-while x.isdigit() == False:
+# lstrip('-') permet de tester les negatives numbers
+while x.lstrip('-').isdigit() == False:
     for c in d.keys():
         if c in x:
-            print('In the if : ' + c, file=sys.stderr, flush=True)
             #on remplace la clé par sa valeur
             x = x.replace(c,d[c])
-            print('X replaced : ' + x, file=sys.stderr, flush=True)
             break
 
 print(x)
