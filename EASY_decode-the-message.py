1e48b1eb8af070d01498c3ccc9826c7193334227
soroda <sdahalani@gmail.com>
2022-05-11 21:58:42 +0200

    decode the message : 4 tests over 7

--- EASY_decode-the-message.py
+++ EASY_decode-the-message.py
@@ -3,32 +3,36 @@ import sys
 
 encoded_value = int(input())
 print(str(encoded_value), file=sys.stderr, flush=True)
-alphabet = []
-for i in input():
-    alphabet.append(i)
+alphabet = [i for i in input()]
 
 print(alphabet, file=sys.stderr, flush=True)
 decoded_msg = ""
 taille_alphabet = len(alphabet)
-test=""
-for a in alphabet:
-    test += a
 
 # on prend un temoin car la taille de l'alphabet va continuer de grandrir
-temoin = taille_alphabet
+temoin = 0
+depart = 0
 depart_1 = 0
-depart_2 = 0
+# Si true alors depart = 0 & depart_1 = encours Sinon depart = encours & depart_1 = 0
+interrupteur = True
 while temoin <= encoded_value:
     # prendre le premier terme de l'alphabet de base
     #   le concat avec tous le monde y compris lui meme
     #   ainsi de suite avec les termes d'apres
-    for i in range(depart_1,depart_1 + taille_alphabet):
-        for j in range(depart_2,depart_2 + taille_alphabet):
+    for i in range(depart, depart + taille_alphabet):
+        for j in range(depart_1, depart_1 + taille_alphabet):
             alphabet.append(alphabet[j]+alphabet[i])
             temoin += 1
     # toutes les possibilite d'une hierarchie ont ete epuisees
-    if len(alphabet)%taille_alphabet == 0:
-        depart_2 += taille_alphabet
+    if len(alphabet) % taille_alphabet == 0:
+        if interrupteur:
+            depart = depart_1 + taille_alphabet
+            depart_1 = 0
+            interrupteur = False
+        else:
+            depart_1 = depart + taille_alphabet
+            depart = 0
+            interrupteur = True
 
 decoded_msg = alphabet[encoded_value]
 
