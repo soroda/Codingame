38ed6e2788de1c1a99190d676c7de093a40bf151
soroda <sdahalani@gmail.com>
2022-05-13 23:58:03 +0200

    few change in asteroids.py
    Do not pass tests

--- EASY_asteroids.py
+++ EASY_asteroids.py
@@ -6,12 +6,19 @@ w, h, t1, t2, t3 = [int(i) for i in input().split()]
 print('w ' + str(w) + ' h ' + str(h), file=sys.stderr,flush=True)
 print(str(t1) + ' ' + str(t2) + ' ' + str(t3), file=sys.stderr,flush=True)
 
+picture_one = picture_two = ""
+
 for i in range(h):
-    picture = ["."] * w
     first_picture_row, second_picture_row = input().split()
-    #print(first_picture_row, second_picture_row, file=sys.stderr,flush=True)
-    for char in first_picture_row:
-        if char.isalpha(): 
-            picture[second_picture_row.find(char)+1] = char
-    
-    print("".join(picture))
+    picture_one += first_picture_row
+    picture_two += second_picture_row
+
+picture_final = "." * h * w
+
+for i in range(0,len(picture_one),w):
+    print(picture_one[i:i+w], file=sys.stderr, flush=True)
+    #for char in picture_one[i:i+w]:
+        #if char != ".":
+
+for i in range(0,len(picture_two),w):
+    print(picture_two[i:i+w], file=sys.stderr, flush=True)
