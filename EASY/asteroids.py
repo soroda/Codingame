bcfca91c5355d0446b5f908d31c10decec5f7140
soroda <sdahalani@gmail.com>
2022-05-15 23:57:53 +0200

    update asteroids
    pass 6 tests over 10

--- EASY/asteroids.py
+++ EASY/asteroids.py
@@ -7,18 +7,24 @@ print('w ' + str(w) + ' h ' + str(h), file=sys.stderr,flush=True)
 print(str(t1) + ' ' + str(t2) + ' ' + str(t3), file=sys.stderr,flush=True)
 
 picture_one = picture_two = ""
+picture_final = ["."] * w * h
+index_1 = index_2 = variance = index_3 = 0
 
 for i in range(h):
     first_picture_row, second_picture_row = input().split()
     picture_one += first_picture_row
     picture_two += second_picture_row
 
-picture_final = "." * h * w
+for i in range(0,len(picture_one)):
+    if picture_one[i] != '.':
+        print("on est dans le if", file=sys.stderr, flush=True)
+        index_1 = i
+        index_2 = picture_two.find(picture_one[i])
+        variance = index_2 - index_1
+        picture_final[index_2 + variance] = picture_one[i]
 
-for i in range(0,len(picture_one),w):
-    print(picture_one[i:i+w], file=sys.stderr, flush=True)
-    #for char in picture_one[i:i+w]:
-        #if char != ".":
-
-for i in range(0,len(picture_two),w):
-    print(picture_two[i:i+w], file=sys.stderr, flush=True)
+for i in range(0,len(picture_final),w):
+    tmp = ""
+    for char in picture_final[i:i+w]:
+        tmp += char
+    print(tmp)
