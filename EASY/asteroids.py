a99217fc10c2bee79afa38935eb2d96d72077532
soroda <sdahalani@gmail.com>
2022-05-17 23:36:19 +0200

    update asteroids 9 tests /10

--- EASY/asteroids.py
+++ EASY/asteroids.py
@@ -24,11 +24,16 @@ for i in range(0,len(picture_two),w):
 
 print("\n", file=sys.stderr,flush=True)
 
+horizontal = True # l'asteroid voyage horizontalement
+
 for i in range(0,len(picture_one)):
     if picture_one[i] != '.':
         index_1 = i # 0
         index_2 = picture_two.find(picture_one[i]) # 4
         variance = index_2 - index_1 # 4
+
+        # Si faux alors l'asteroid avance verticalement
+        if (index_1//w) != (index_2//w): horizontal = False
 
         if (t2-t1) != (t3-t2):
             temps1 = t3 - t2 # 1
@@ -36,11 +41,20 @@ for i in range(0,len(picture_one)):
             temps = variance // temps0 # 4 // 4 = 1
             variance = temps * temps1 # 1 * 1
         
-        if picture_final[index_2 + variance] != '.':
-            if ord(picture_one[i]) < ord(picture_final[index_2 + variance]):
-                picture_final[index_2 + variance] = picture_one[i]
+        if horizontal :
+            # on sort du cadre (horizontalement) & était horizontal entre t1 & t2
+            if (index_2+variance)//w != index_2//w and index_2//w == index_1//w:
+                continue
         else:
-            picture_final[index_2 + variance] = picture_one[i]
+            # on sort du cadre (verticalement) & était verticale entre t1 & t2
+            if (index_2+variance)%w != index_2%w and index_2%w == index_1%w:
+                continue
+        if index_2 + variance < len(picture_final):
+            if picture_final[index_2 + variance] != '.':
+                if ord(picture_one[i]) < ord(picture_final[index_2 + variance]):
+                    picture_final[index_2 + variance] = picture_one[i]
+            else:
+                picture_final[index_2 + variance] = picture_one[i]
             
 for i in range(0,len(picture_final),w):
     tmp = ""
