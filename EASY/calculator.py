6cfec01ffd1937e0c0ec4f4a37684e0a4c9ba327
soroda <sdahalani@gmail.com>
2022-05-22 22:54:18 +0200

    calculator 2 tests over 7

--- EASY/calculator.py
+++ EASY/calculator.py
@@ -3,23 +3,43 @@ import sys
 import math
 terme1=""
 terme2=""
-res=0
+signe=""
+answer=""
+
+def calculate(t1,t2,s):
+    res = 0
+    t1 = int(t1)
+    t2 = int(t2)
+    if s=='+': res=t1+t2
+    if s=='-': res=t1-t2
+    if s=='/': res=t1/t2
+    if s=='x': res=t1*t2
+    return str(res)
+
 for i in range(int(input())):
     key = input()
-    if key.isdigit() and terme2 == "":
-        terme1 += str(key)
-        print(terme1)
-    elif key.isdigit and terme2 != "":
-        terme2 += str(key)
-        print(terme2[1:])
-    elif key in "+-/*":
-        terme2=key
-        print(terme1)
-    elif key == "=":
-        continue
-    elif key=="AC":
+    print("->",key, file=sys.stderr,flush=True)
+    if key.isdigit():
+        if terme1=="" or (terme1 != "" and signe==""):
+            terme1+= key
+            answer = terme1
+        elif signe!="" and terme1 !="":
+            terme2+= key
+            answer = terme2
+    elif key=='AC':
+        terme1=""
+        terme2=""
+        signe=""
+        answer="0"
+    elif key == '=':
+        answer = calculate(terme1,terme2,signe)
         terme1 = ""
-        terme2 = ""
-        res = 0
-        print(str(res))
-    
+    elif key in "+/-x":
+        if terme2 != "":
+            answer = calculate(terme1,terme2,signe)
+            terme2 = ""
+        signe=key
+        terme1 = answer
+    else:
+        answer = calculate(terme1,terme2,signe)
+    print(answer)
