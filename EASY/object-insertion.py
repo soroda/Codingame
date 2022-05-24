f408c082a4a4374dbc4e14d9ab6043a98ada29e4
soroda <sdahalani@gmail.com>
2022-05-24 23:55:17 +0200

    object insertion 1 test over 7

--- EASY/object-insertion.py
+++ EASY/object-insertion.py
@@ -1,20 +1,39 @@
 # https://www.codingame.com/ide/puzzle/object-insertion
 import sys
-import math
 
 a, b = [int(i) for i in input().split()]
+
 print("a",a,"b",b, file=sys.stderr, flush=True)
+
 objet = []
 grid = []
+solution = []
+nombre_solution=0
+
 for i in range(a):
     objet.append(list(input()))
+print("objet",objet, file=sys.stderr, flush=True)
 
 c, d = [int(i) for i in input().split()]
 print("c",c,"d",d, file=sys.stderr, flush=True)
 
 for i in range(c):
-    grid.append(list(input()))
+    tmp = list(input())
+    grid.append(tmp)
+    solution.append(tmp)
 
-print(0)
-for i in range(c):
-    print("".join(grid[i]))
+print("grid",grid, file=sys.stderr, flush=True)
+# premiere etape calculer le nombre de possibilié
+
+# mémoriser la premiere possibilité. 
+
+# le nombre de possibilité est supérieur à 1 alors afficher le nombre
+# Si nombre de possibilité est égal à 1 alors afficher la grille
+# Si aucune solution ne rien afficher
+
+if nombre_solution == 1 :
+    print(nombre_solution)
+    for i in range(c):
+        print("".join(solution[i]))
+else:
+    print(nombre_solution)
