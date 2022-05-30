c22212bd1806078968a727c8f4039101f51fb290
soroda <sdahalani@gmail.com>
2022-05-31 00:12:40 +0200

    survey prediction 4/4 but 2/4 with validators

--- EASY/survey-prediction.py
+++ EASY/survey-prediction.py
@@ -1,16 +1,17 @@
 # https://www.codingame.com/ide/puzzle/survey-prediction
 
-import sys
-
-stat = {"female":{},"male":{}}
 # {female:{23:"hip-hop",30:hip-hop}}
+stat = {"female":{},"male":{}}
 
 for i in range(int(input())):
     line = input().split()
-    print(line, file=sys.stderr, flush=True)
     if len(line)==3:
         stat[line[1]][int(line[0])] = line[2]
     else:
-        for age in stat[line[1]]:
-            if age >= int(line[0]):
-                print(stat[line[1]][age])
+        if int(line[0]) > max(stat[line[1]]):
+            print("None")
+        else:
+            for age in sorted(stat[line[1]]):
+                if age >= int(line[0]):
+                    print(stat[line[1]][age])
+                    break
