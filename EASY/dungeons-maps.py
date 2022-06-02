f8f15e6400f443b08b3cb0f1fa5e5c4b3e6ea485
soroda <sdahalani@gmail.com>
2022-06-02 23:08:51 +0200

    dungeons-maps all tests ok

--- EASY/dungeons-maps.py
+++ EASY/dungeons-maps.py
@@ -1,36 +1,47 @@
 # https://www.codingame.com/ide/puzzle/dungeons-and-maps
-import sys
 
 mapIndex = -1
-
 w, h = [int(i) for i in input().split()]
-
 shortest_path = w * h
-
 row, col = [int(i) for i in input().split()]
 
-print("start_row", row, "start_col", col, file=sys.stderr, flush=True)
-
-def testValidPath(mr):
-    counter=0
-    # fonction recursive pour suivre le chemin
-    return 0
+def testValidPath(mr, a, b, prec) -> int:
+    if prec != '+' and a == row and b == col:
+        return ((w * h) + 1) * -1
+    
+    if a == h or a < 0 or b == w or b < 0:
+        return ((w * h) + 1) * -1
+    
+    if mr[a][b] == '>':
+        if prec in '>v^+':
+            return testValidPath(mr,a,b+1,mr[a][b]) + 1
+    elif mr[a][b] == 'v':
+        if prec in 'v><+':
+            return testValidPath(mr,a+1,b,mr[a][b]) + 1
+    elif mr[a][b] == '^':
+        if prec in '^><+':
+            return testValidPath(mr,a-1,b,mr[a][b]) + 1
+    elif mr[a][b] == '<':
+        if prec in '<v^+':
+            return testValidPath(mr,a,b-1,mr[a][b]) + 1
+    elif mr[a][b] == 'T':
+        if prec in '<>^v':
+            return 1
+    else:
+        return ((w * h) + 1) * -1 
 
 for i in range(int(input())):
-    print(i, file=sys.stderr, flush=True)
     map_row = []
     for j in range(h):
         map_row.append(input())
-    for line in map_row:
-        print(line, file=sys.stderr, flush=True)
-    print("", file=sys.stderr, flush=True)
 
     # test if there is valid path
-    path_length = testValidPath(map_row)
-    # if short than shortest_path then map_index=i
+    # + is for indicating that's the start operation
+    path_length = testValidPath(map_row, row, col,'+')
+    # if short than shortest_path then mapIndex=i
     if 0 < path_length < shortest_path : 
         shortest_path = path_length
-        map_index = i
+        mapIndex = i
 
 if mapIndex < 0:
     print("TRAP")
