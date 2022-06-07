0f53ecc3942b0baf50c5f0b27fbda2a2ee697905
soroda <sdahalani@gmail.com>
2022-06-07 23:23:04 +0200

    orderofSuccession : add dictonaries

--- EASY/order-of-succession.py
+++ EASY/order-of-succession.py
@@ -1,16 +1,27 @@
 # https://www.codingame.com/ide/puzzle/order-of-succession
 import sys
-import math
 
 answer = "orDeroFsucceSsion"
+child = {} # child -> parent
+parents = {} # parent : [childs]
+people = {}
 
 for i in range(int(input())):
     inputs = input().split()
     print("inputs", inputs, file=sys.stderr, flush=True)
     name = inputs[0]
-    print(name)
     parent = inputs[1]
     birth = int(inputs[2])
     death = inputs[3]
     religion = inputs[4]
     gender = inputs[5]
+
+    child[name] = parent
+    if name in parents.keys():
+        parents[parent].append(name)
+    else:
+        parents[parent] = [].append(name)
+
+    people[name] = list(inputs)
+
+print(parents["-"])
