833e08fff40c9f47d51e93352340fca655b18424
soroda <sdahalani@gmail.com>
2022-06-09 23:56:52 +0200

    metric units : all test ok

--- EASY/metric-units.py
+++ EASY/metric-units.py
@@ -1,14 +1,32 @@
 # https://www.codingame.com/ide/puzzle/metric-units
-import sys
 import re
 
+units = [1,1000,10,10,10,1000] # 0 um 1 mm 2 cm 3 dm 4 m 5 km
+rank = {'um':0,'mm':1,'cm':2,'dm':3,'m':4,'km':5}
+
 value1, value2 = input().split('+')  # the expression to calculate
-unit1 = re.sub(r'\d','',value1)
-unit2 = re.sub(r'\d','',value2)
+unit1 = re.sub(r'\d','',value1).replace(' ','').replace('.','')
+unit2 = re.sub(r'\d','',value2).replace(' ','').replace('.','')
 
-value1 = int(re.sub(r'[a-z]*','',value1))
-value2 = int(re.sub(r'[a-z]*','',value2))
+value1 = float(re.sub(r'[a-z]*','',value1))
+value2 = float(re.sub(r'[a-z]*','',value2))
 
-print("value1",value1,"unit1",unit1,"unit2",unit2,"value2",value2, file=sys.stderr, flush=True)
+def convert(u1,u2,v):
+    res = v
+    for i in range(rank[u1],rank[u2], -1):
+        res *= units[i]
+    return round(res,5)
 
-print("1cm")
+answer = 0.0
+
+if rank[unit1] > rank[unit2]:
+    answer = convert(unit1,unit2,value1) + value2
+    unit1 = unit2
+elif rank[unit2] > rank[unit1]:
+    answer = convert(unit2,unit1,value2) + value1
+else:
+    answer = value1+value2
+
+if answer == int(answer): answer = int(answer)
+
+print(str(answer) + unit1)
