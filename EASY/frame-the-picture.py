c3be78ce85cd646b0c4a1311f4ab4a78bb4ba5db
soroda <sdahalani@gmail.com>
2022-06-16 23:10:26 +0200

    Update frame-the-picture.py

--- EASY/frame-the-picture.py
+++ EASY/frame-the-picture.py
@@ -3,12 +3,32 @@ import sys
 
 frame_pattern = input()  # the ASCII art pattern to use to frame the picture
 print("frame_pattern", frame_pattern, file=sys.stderr, flush=True)
-# h: the height of the picture
-# w: the width  of the picture
 h, w = [int(i) for i in input().split()]
 print("h", h, "w", w, frame_pattern, file=sys.stderr, flush=True)
-for i in range(h):
-    line = input()  # the ASCII art picture line by line
-    print("line", line, file=sys.stderr, flush=True)
 
-print("Write framed picture line by line here")
+banners = []
+reversedFramePattern = frame_pattern[::-1] # slicer
+
+lines = []
+for i in range(h):
+    lines.append(frame_pattern + (w * " ") + input() +  (w * " ") + reversedFramePattern)
+
+lineSize = max([len(x) for x in lines])
+
+for char in frame_pattern:
+    banners.append(char * lineSize)
+
+between = frame_pattern + (lineSize - (2*len(frame_pattern))) * " " + reversedFramePattern
+
+for b in banners:
+    print(b)
+
+print(between)
+
+for l in lines:
+    print(l)
+
+print(between)
+
+for b in banners[::-1]:
+    print(b)
