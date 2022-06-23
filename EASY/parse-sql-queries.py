d02a57d7402b44e4cb9aee42e193be3df699406e
soroda <sdahalani@gmail.com>
2022-06-23 22:39:20 +0200

    Update parse-sql-queries.py

--- EASY/parse-sql-queries.py
+++ EASY/parse-sql-queries.py
@@ -2,9 +2,37 @@
 import sys
 
 sql_query = input()
+
+print(sql_query, file=sys.stderr, flush=True)
+
 rows = int(input())
-table_header = input().split()
-print(" ".join(table_header))
+
+table = []
+header = {}
+
+for c,v in enumerate(input().split()):
+    header[v]=c
+
 for i in range(rows):
-    table_row = input()
-    print(table_row)
+    table.append(input().split())
+
+s, f, w = False, False, False
+selected = []
+
+for t in sql_query:
+    if t == "SELECT": 
+        s = True
+        continue
+    if t == "FROM": 
+        s = False
+        continue
+    if t == "WHERE": 
+        w = True
+        continue
+    
+    if s:
+        if t=='*':
+            selected = [k for k in header.keys()]
+        else:
+            selected.append(t)
+    
