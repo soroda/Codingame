c7ed05a9853f89c0a1d7c9eb64bb89125436659d
soroda <sdahalani@gmail.com>
2022-06-28 13:36:01 +0200

    Unique prefix : all test ok

--- EASY/unique-prefixes.py
+++ EASY/unique-prefixes.py
@@ -1,35 +1,34 @@
 # https://www.codingame.com/ide/puzzle/unique-prefixes
-import sys
-
 words, prefix = [], []
 
 for i in range(int(input())):
     words.append(input())
 
 index = 0
+unique = []
+
 while index < len(words):
     pref, tmp = "", ""
     for i in range(0, len(words),1):
         tmp = ""
         j = 0   
         if i != index: #compare avec tous sauf lui meme
-            print(words[index],'vs',words[i],file=sys.stderr,flush=True)     
             for c in words[index]:
                 if j < len(words[i]):
-                    print(c,'vs',words[i][j],file=sys.stderr,flush=True)
                     if c == words[i][j]:
                         tmp += c
                         j+=1
+                        if len(tmp) == 1:unique.append(tmp)
                     else:
-                        if len(tmp) > 1:
+                        if len(tmp) > 1 or tmp in unique:
                             tmp+=c
                         break
-                elif len(tmp) > 1:
+                elif len(tmp) > 1 or tmp in unique:
                     tmp+=c
                     break
             if len(tmp) >= len(pref): pref = tmp
             if len(tmp) == len(pref) == 0:pref=words[index][0]
-            print('pref:',pref,'tmp:',tmp,file=sys.stderr,flush=True)
+            if len(tmp) == 1:unique.append(tmp)
     index += 1
     prefix.append(pref)
 
