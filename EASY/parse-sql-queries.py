194e39337ce87323e3e980cadd35887abe6a0094
soroda <sdahalani@gmail.com>
2022-06-30 14:02:39 +0200

    Update parse-sql-queries.py

--- EASY/parse-sql-queries.py
+++ EASY/parse-sql-queries.py
@@ -1,6 +1,5 @@
 # https://www.codingame.com/ide/puzzle/parse-sql-queries
 import sys
-import numpy as np
 
 sql_query = input().split()
 
@@ -13,11 +12,33 @@ for i in range(rows):
     table.append(input().split())
 
 query_select = False
+query_from=False
 query_where = False
+selected_index=[]
 # requete decoupee
 for t in sql_query:
     print(t, file=sys.stderr, flush=True)
-    if t == 'SELECT':query_select=True
-    if t == 'FROM':query_select=False
-    if t == 'WHERE':query_where=True
+    tmp = []
+    if t == 'SELECT':
+        query_select=True
+        continue
+    if t == 'FROM':
+        query_select=False
+        query_from=True
+        continue
+    if t == 'WHERE':
+        query_from=False
+        query_where=True
+        continue
 
+    if query_select:
+        if t=='*': tmp = table[0]
+        tmp.append(t)
+    
+    if query_from:
+        a = 0
+        for i in tmp:
+            while i != table[0][a]:
+                a+=1
+            
+            selected_index.append(a)
